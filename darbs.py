ff3ef82602cc3d64d8536d3edcbbf1c8f0c66176
JekabsBelavskiss <jekabsb20@gmail.com>
2024-10-18 17:10:49 +0300

    Update darbs.py

--- darbs.py
+++ darbs.py
@@ -1,4 +1,4 @@
-def ievadit_ienakumus_un_izdevumus():
+def ievade():
     ienakumi = float(input("Ievadiet uzņēmuma ienākumus: "))
     izdevumi = float(input("Ievadiet uzņēmuma izdevumus: "))
     return ienakumi, izdevumi
@@ -6,19 +6,19 @@ def ievadit_ienakumus_un_izdevumus():
 def aprekikins(ienakumi, izdevumi, nlikme):
     pelna = ienakumi - izdevumi
     nodoklis = pelna * (nlikme / 100)
-    neto_pelna = pelna - nodoklis
-    return pelna, nodoklis, neto_pelna
+    netopelna = pelna - nodoklis
+    return pelna, nodoklis, netopelna
 
 def programma():
     for _ in range(1): 
-        ienakumi, izdevumi = ievadit_ienakumus_un_izdevumus()
+        ienakumi, izdevumi = ievade()
 
         if izdevumi > ienakumi:
             print("Kļūda: Izdevumi nevar būt lielāki par ienākumiem.")
         else:
             nlikme = float(input("Ievadiet nodokļu (%): "))
-            pelna, nodoklis, neto_pelna = aprekikins(ienakumi, izdevumi, nlikme)
-            print(f"Peļņa pēc nodokļiem: {neto_pelna:.2f} EUR")
+            pelna, nodoklis, netopelna = aprekikins(ienakumi, izdevumi, nlikme)
+            print(f"Peļņa pēc nodokļiem: {netopelna:.2f} EUR")
             print(f"Nodokļi: {nodoklis:.2f} EUR")
             print(f"Bruto peļņa: {pelna:.2f} EUR")
 
